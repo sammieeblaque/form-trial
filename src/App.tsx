c8645a4f81f8550d06ec405df1985c9a36fb26b3
Sammiee <88675348+sammieeblaque@users.noreply.github.com>
2025-07-24 20:47:08 +0100

    chore: comment app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import "./App.css";
-import JsonformBuilder from "./form-builder/Form";
+// import "./App.css";
+// import JsonformBuilder from "./form-builder/Form";
 
-function App() {
-  return (
-    <>
-      <h1 className="text-2xl font-bold mb-4"></h1>
-      <JsonformBuilder />
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       <h1 className="text-2xl font-bold mb-4"></h1>
+//       <JsonformBuilder />
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
